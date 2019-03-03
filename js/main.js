267e497d430bf99993cf63cd354951ac60704abe
Pati <Patrycjaqtno05@gmail.com>
2019-03-03 20:29:52 +0100

    I created gestalt and price site + references

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$('#Read_more').click(function () {
+    $('#Gestalt').css('display', 'block'),
+        $('#Cennik').css('display', 'block'),
+        $('#Home').css('display', 'none'),
+        $('#Offer').css('display', 'none'),
+        $('#Kontakt').css('display', 'none')
+});
+$('#Home-button').click(function () {
+    $('#Gestalt').css('display', 'none'),
+        $('#Cennik').css('display', 'none'),
+        $('#Home').css('display', 'block'),
+        $('#Offer').css('display', 'block'),
+        $('#Kontakt').css('display', 'block')
+});
+$('#Offer-button').click(function () {
+    $('#Gestalt').css('display', 'none'),
+        $('#Cennik').css('display', 'none'),
+        $('#Home').css('display', 'block'),
+        $('#Offer').css('display', 'block'),
+        $('#Kontakt').css('display', 'block')
+});
+$('#Kontakt-button').click(function () {
+    $('#Gestalt').css('display', 'none'),
+        $('#Cennik').css('display', 'none'),
+        $('#Home').css('display', 'block'),
+        $('#Offer').css('display', 'block'),
+        $('#Kontakt').css('display', 'block')
+});
+$('#Gestalt-button').click(function () {
+    $('#Gestalt').css('display', 'block'),
+        $('#Cennik').css('display', 'block'),
+        $('#Home').css('display', 'none'),
+        $('#Offer').css('display', 'none'),
+        $('#Kontakt').css('display', 'none')
+});
+$('#Cennik-button').click(function () {
+    $('#Gestalt').css('display', 'block'),
+        $('#Cennik').css('display', 'block'),
+        $('#Home').css('display', 'none'),
+        $('#Offer').css('display', 'none'),
+        $('#Kontakt').css('display', 'none')
+});
+
