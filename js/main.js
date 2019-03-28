beab9782bfa2fd15aebd468df58bae3f6da22628
Pati <Patrycjaqtno05@gmail.com>
2019-03-28 21:19:08 +0100

    little change

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 ////*************************Animations */
+
 window.sr = ScrollReveal();
 sr.reveal('.info, .photo,#services1,#services2,#info1-gestalt,#info2-gestalt,#info3-gestalt', { delay: 200, reset: true, duration: 2000 });
 /////OfferSite
