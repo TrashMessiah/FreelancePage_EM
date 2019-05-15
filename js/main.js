d1d0d7008a09cb159c9bf903b3540e84d5961f4b
Pati <Patrycjaqtno05@gmail.com>
2019-05-15 23:30:09 +0200

    Compressed
    minify html,css and js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,1 @@
-////*************************Animations */
-
-window.sr = ScrollReveal();
-sr.reveal('.info, .photo,#services1,#services2,#info1-gestalt,#info2-gestalt,#info3-gestalt', { delay: 200, reset: true, duration: 2000 });
-/////OfferSite
-sr.reveal('#photo-offer, #info-gestalt', { delay: 200, reset: true, duration: 2000 });
-/////GestaltSite
-sr.reveal('#info-cennik, #photo-gestalt', { delay: 200, reset: true, duration: 2000 });
-////PriceSite
-sr.reveal('#photo-price,#info-price', { delay: 200, reset: true, duration: 2000 });
-
-$('.navbar-nav>li>a').on('click', function () {
-    $('.navbar-collapse').collapse('hide');
-});
+window.sr = ScrollReveal(), sr.reveal(".info, .photo,#services1,#services2,#info1-gestalt,#info2-gestalt,#info3-gestalt", { delay: 200, reset: !0, duration: 2e3 }), sr.reveal("#photo-offer, #info-gestalt", { delay: 200, reset: !0, duration: 2e3 }), sr.reveal("#info-cennik, #photo-gestalt", { delay: 200, reset: !0, duration: 2e3 }), sr.reveal("#photo-price,#info-price", { delay: 200, reset: !0, duration: 2e3 }), $(".navbar-nav>li>a").on("click", function () { $(".navbar-collapse").collapse("hide") });
