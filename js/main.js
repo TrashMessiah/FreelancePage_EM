5d54d4007fba975cbe586c8d6491527ec2173fc0
Patrycja <Patrycjaqtno05@gmail.com>
2022-06-19 16:20:15 +0200

    add booker

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
-window.sr = ScrollReveal(), sr.reveal(".info, .photo,#services1,#services2,#info1-gestalt,#info2-gestalt,#info3-gestalt", { delay: 200, reset: !0, duration: 2e3 }), sr.reveal("#photo-offer, #info-gestalt", { delay: 200, reset: !0, duration: 2e3 }), sr.reveal("#info-cennik, #photo-gestalt", { delay: 200, reset: !0, duration: 2e3 }), sr.reveal("#photo-price,#info-price", { delay: 200, reset: !0, duration: 2e3 }), $(".navbar-nav>li>a").on("click", function () { $(".navbar-collapse").collapse("hide") });
+(window.sr = ScrollReveal()),
+  sr.reveal(
+    ".info, .photo,#services1,#services2,#info1-gestalt,#info2-gestalt,#info3-gestalt",
+    { delay: 200, reset: !0, duration: 2e3 }
+  ),
+  sr.reveal("#photo-offer, #info-gestalt", {
+    delay: 200,
+    reset: !0,
+    duration: 2e3,
+  }),
+  sr.reveal("#info-cennik, #photo-gestalt", {
+    delay: 200,
+    reset: !0,
+    duration: 2e3,
+  }),
+  sr.reveal("#photo-price,#info-price", {
+    delay: 200,
+    reset: !0,
+    duration: 2e3,
+  }),
+  $(".navbar-nav>li>a").on("click", function () {
+    $(".navbar-collapse").collapse("hide");
+  });
+
+setTimeout(() => {
+  let bookero = document.getElementsByClassName("bookero-sticky-plugin-toggle");
+
+  bookero[1].innerHTML = "Zamów wizytę";
+  bookero[1].style.display = "block";
+}, "1000");
